7c44cd30b132e57c6610c152fa3578040c5e3814
Mateusz <kacpermateo@gmail.com>
2025-01-15 17:26:55 +0100

    praca nad projektem 002

--- projekt_zaliczenie_1/projekt_zaliczenie/myapp/admin.py
+++ projekt_zaliczenie_1/projekt_zaliczenie/myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Stanowisko, Osoba, Produkt, User
 # Register your models here.
+class OsobaAdmin(admin.ModelAdmin):
+    readonly_fields = ('data_dodania',)
+
+admin.site.register(Osoba)
+admin.site.register(Stanowisko) 
+admin.site.register(Produkt)
+admin.site.register(User)
--- projekt_zaliczenie_1/projekt_zaliczenie/myapp/models.py
+++ projekt_zaliczenie_1/projekt_zaliczenie/myapp/models.py
@@ -1,3 +1,62 @@
 from django.db import models
-
+from django.utils import timezone
+from django.db.models import IntegerChoices
 # Create your models here.
+
+
+# deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
+SHIRT_SIZES = [
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+]
+
+class plec(IntegerChoices): 
+    mezczyzna = 1, 'Mężczyzna'
+    kobieta = 2, 'Kobieta'
+    inne = 3, 'Inne'
+
+
+class Osoba(models.Model):
+    imie = models.CharField(max_length=60, blank=False)
+    nazwisko = models.CharField(max_length=60, blank=False)
+    plec = models.IntegerField(choices=plec.choices)
+    stanowisko = models.ForeignKey('Stanowisko', on_delete=models.CASCADE)
+    data_dodania = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko}"
+    
+    class Meta:
+        verbose_name_plural = "Osoby"
+
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=60, blank=False)
+    opis = models.TextField(max_length=60, blank=True)
+    def __str__(self):
+        return self.nazwa
+    
+    class Meta:
+        verbose_name_plural = "Stanowiska"
+    
+
+
+class Produkt(models.Model):
+    name = models.CharField(max_length=60)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Produkty'
+    
+class User(models.Model):
+    name = models.CharField(max_length=60)
+    email = models.EmailField()
+    password = models.CharField(max_length=60)
+    def __str__(self):
+        return self.name
+    
