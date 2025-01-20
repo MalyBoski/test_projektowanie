9b933b40b9b4527b70a64c1a3524efb243dee62c
Mateusz <kacpermateo@gmail.com>
2025-01-20 23:33:00 +0100

    Poprawki zajeciowe (lista zamowien wedlug sortowania daty, glowna strona) 017

--- projekt_zaliczenie_1/projekt_zaliczenie/API/urls.py
+++ projekt_zaliczenie_1/projekt_zaliczenie/API/urls.py
@@ -1,22 +1,25 @@
-from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
-from.views import SongList, CreateSongView, UpdateASongView, deleteSongView, SongViewSet, RegisterView, CustomLoginView, CartView, AlbumViewSet, AlbumsByLetterView, AllOrderView, PurchaseView, CreateOrderView, OrderDetailView
+from django.urls import path 
+from.views import CreateSongView, UpdateASongView, deleteSongView, RegisterView, CustomLoginView, CartView, AlbumsByLetterView, AllOrderView, PurchaseView, CreateOrderView, OrderDetailView, main_page_view, song_list_view
 from . import views
 
 urlpatterns = [
+    # Uzytkownika opcje (basic user)
     path('create_order/', CreateOrderView.as_view(), name='create_order'),
     path('order/<int:order_id>/', OrderDetailView.as_view(), name='order_detail'),
     path('cart/purchase/', PurchaseView.as_view(), name="cart-purchase"),
     path('cart/', CartView.as_view(), name='cart'), 
     path('cart/add/', CartView.as_view(), name='add_to_cart'),
     path('albums/', views.album_list, name='album_list'),
-    path('orders/all/', AllOrderView.as_view(), name='all-orders'),
     path('albums/by-letter/<str:letter>/', AlbumsByLetterView.as_view(), name='albums-by-letter'),
-    path('songs/', SongList.as_view(), name='song-list'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('main/', main_page_view, name='main_page'),  
+    path('songs/', song_list_view, name='song_list'),
+    # Admin opcje
+    # Mozliwosc wybierania zamowien z okreslonej daty
+    # http://127.0.0.1:8000//API/orders/all/?start_date=rok-miesiac-dzien&end_date=rok-miesiac-dzien    (np. 2023-01-31)
+    path('orders/all/', AllOrderView.as_view(), name='all-orders'), 
     path('songs/create/', CreateSongView.as_view()),
     path('songs/<int:pk>/update/', UpdateASongView.as_view()),
     path('songs/<int:pk>/delete/', deleteSongView.as_view()),
-    path('songs/', SongViewSet.as_view({'get': 'list'})),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', CustomLoginView.as_view(), name='custom_login'),    
 ]
